b3e9f162642a9a8801eedafbcd036a189775e59a
Josh Wolfe <thejoshwolfe@gmail.com>
2010-09-16 22:57:27 -0700

    formatted test.py and added -q

--- test.py
+++ test.py
@@ -12,69 +12,80 @@ def superwalk(folder):
 def absolute(relative_path):
     return os.path.abspath(os.path.join(os.path.dirname(__file__), relative_path))
 
-parser = optparse.OptionParser()
-parser.add_option('-f', '--failfast', dest='failfast', help="Stop on first failed test", metavar="FAILFAST", action="store_true")
-options, args = parser.parse_args()
 
-print("Loading test suite...")
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option('-f', '--failfast', dest='failfast', help="Stop on first failed test", action="store_true")
+    parser.add_option("-q", "--quiet", help="only print dots and summary", action="store_true")
+    options, args = parser.parse_args()
 
-tests = {}
-for f in superwalk('tests/formal'):
-    if f.endswith('.p.expected'):
-        test_name = f[:-len('.p.expected')]
-        ext = '.p.expected'
-    elif f.endswith('.p'):
-        test_name = f[:-len('.p')]
-        ext = '.p'
-    else:
-        continue
+    if not options.quiet:
+        print("Loading test suite...")
 
-    if not tests.has_key(test_name):
-        tests[test_name] = {}
+    tests = {}
+    for f in superwalk('tests/formal'):
+        if f.endswith('.p.expected'):
+            test_name = f[:-len('.p.expected')]
+            ext = '.p.expected'
+        elif f.endswith('.p'):
+            test_name = f[:-len('.p')]
+            ext = '.p'
+        else:
+            continue
 
-    if ext == '.p.expected':
-        tests[test_name]['expected'] = open(absolute(f), 'r').read()
-    else:
-        tests[test_name]['source'] = open(absolute(f), 'r').read()
+        if not tests.has_key(test_name):
+            tests[test_name] = {}
 
-fails = []
-compiler_exe = absolute('opc')
-passed = 0
-for test_name, test in tests.iteritems():
-    if not test.has_key('source'):
-        print("%s missing source" % test_name)
-        continue
-    if not test.has_key('expected'):
-        print("%s missing expected value" % test_name)
-        continue
+        if ext == '.p.expected':
+            tests[test_name]['expected'] = open(absolute(f), 'r').read()
+        else:
+            tests[test_name]['source'] = open(absolute(f), 'r').read()
 
-    compiler = subprocess.Popen([compiler_exe], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = compiler.communicate(test['source'])
-    if stderr == test['expected']:
-        sys.stdout.write('.')
-        passed += 1
-    else:
-        sys.stdout.write('F')
-        fails.append({
-            'expected': test['expected'],
-            'output': stderr,
-            'name': test_name,
-        })
-        if options.failfast:
-            break
-    sys.stdout.flush()
+    fails = []
+    compiler_exe = absolute('opc')
+    passed = 0
+    for test_name, test in tests.iteritems():
+        if not test.has_key('source'):
+            print("%s missing source" % test_name)
+            continue
+        if not test.has_key('expected'):
+            print("%s missing expected value" % test_name)
+            continue
 
-if len(fails) > 0:
-    print("\n=========================================")
-    for fail in fails:
-        print("""Test name: %(name)s
+        compiler = subprocess.Popen([compiler_exe], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = compiler.communicate(test['source'])
+        if stderr == test['expected']:
+            sys.stdout.write('.')
+            passed += 1
+        else:
+            sys.stdout.write('F')
+            fails.append({
+                'expected': test['expected'],
+                'output': stderr,
+                'name': test_name,
+            })
+            if options.failfast:
+                break
+        sys.stdout.flush()
+
+
+
+    if len(fails) > 0:
+        if not options.quiet:
+            print("\n=========================================")
+            for fail in fails:
+                print("""Test name: %(name)s
 ---- Output: ----
 %(output)s
 ---- Expected output: ----
 %(expected)s
 --------
 """ % fail)
-        print("=========================================")
-    print("\n%i tests passed, %i tests failed." % (passed, len(fails)))
-else:
-    print("\nAll %i tests passed." % len(tests))
+                print("=========================================")
+        print("\n%i tests passed, %i tests failed." % (passed, len(fails)))
+    else:
+        print("\nAll %i tests passed." % len(tests))
+
+if __name__ == "__main__":
+    main()
+
